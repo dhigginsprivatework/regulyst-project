26d07a308b1cf69857121f45221ed16d2ecd5774
diarmuidhiggins1 <Diarmuid.Higgins@boi.com>
2025-07-26 22:16:52 +0100

    LWC UI Enhancements

--- force-app/main/default/lwc/fieldConfig/fieldConfig.js
+++ force-app/main/default/lwc/fieldConfig/fieldConfig.js
@@ -1,9 +1,5 @@
 export const fieldConfig = {
   Project__c: [
-    { label: 'Framework', fieldName: 'Framework__r.Name', isLookup: true, objectApiName: 'Framework__c' },
-    { label: 'Project Focus', fieldName: 'Project_Focus__c' },
-    { label: 'Standards Alignment Body', fieldName: 'Standards_Alignment_Body__c' },
-    { label: 'Standards Alignment Framework', fieldName: 'Standards_Alignment_Framework__c' },
     { label: 'Description', fieldName: 'Description__c', highlight: true }
   ],
   Project_Framework__c: [
