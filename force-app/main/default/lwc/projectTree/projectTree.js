99e7eeb622b6e443ed374686d3ae8ed9ca695b33
diarmuidhiggins1 <Diarmuid.Higgins@boi.com>
2025-07-27 11:29:58 +0100

    Update ProjectTree LWC to handle opening records directly from the tree

--- force-app/main/default/lwc/projectTree/projectTree.js
+++ force-app/main/default/lwc/projectTree/projectTree.js
@@ -9,6 +9,7 @@ export default class ProjectTree extends NavigationMixin(LightningElement) {
     wiredTree({ error, data }) {
         if (data) {
             this.treeData = this.formatTree(data);
+            console.log('this.treeData', JSON.stringify(this.treeData));
         } else if (error) {
             console.error('Error loading tree:', error);
         }
@@ -24,12 +25,20 @@ export default class ProjectTree extends NavigationMixin(LightningElement) {
     }
 
     handleSelect(event) {
-        const nodeId = event.detail.name;
-        const node = this.findNodeById(this.treeData, nodeId);
-        if (node) {
+    const nodeId = event.detail.name;
+    const node = this.findNodeById(this.treeData, nodeId);
+    if (node) {
+        console.log('Navigating to:', node.name, node.sObjectType);
+
+        try {
             thisNavigationMixin.Navigate;
+        } catch (e) {
+            console.error('NavigationMixin failed, falling back to URL:', e);
+            window.open(`/lightning/r/${node.sObjectType}/${node.name}/view`, '_blank');
         }
     }
+}
+
 
     findNodeById(nodes, id) {
         for (let node of nodes) {
