e132c1bad44c9261257902cb779b732be04d32fc
diarmuidhiggins1 <Diarmuid.Higgins@boi.com>
2025-07-31 12:08:27 +0100

    Framework Admin Enhancements

--- force-app/main/default/lwc/frameworkTile/frameworkTile.js
+++ force-app/main/default/lwc/frameworkTile/frameworkTile.js
@@ -4,11 +4,19 @@ export default class FrameworkTile extends LightningElement {
     @api framework;
 
     get isIsoFramework() {
-        return this.framework.Name && this.framework.Name.includes('ISO 27001');
+        return this.framework.Name && this.framework.Name.includes('ISO 27001:2022 Controls Mapping to Information Security Control Domains');
+    }
+
+    get isNistIsoFramework() {
+        return this.framework.Name && this.framework.Name.includes('NIST CSF 2.0 and ISO 27001:2022');
     }
 
     get isoImageUrl() {
-        return '/resource/ISO_27001_Logo'; // Replace with your static resource name
+        return '/resource/ISO_27001_Logo'; // Replace with your actual static resource name
+    }
+
+    get nistIsoImageUrl() {
+        return '/resource/NISTISO27001Logo'; // Replace with your actual static resource name
     }
 
     handleClick() {
