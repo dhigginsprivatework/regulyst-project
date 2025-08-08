17f6d2eb3e4fdd125913cc018f685923ae747bbc
diarmuidhiggins1 <Diarmuid.Higgins@boi.com>
2025-08-08 16:48:31 +0100

    Project Team Enhancements

--- force-app/main/default/lwc/projectPanelLauncher/projectPanelLauncher.js
+++ force-app/main/default/lwc/projectPanelLauncher/projectPanelLauncher.js
@@ -2,18 +2,18 @@ import { LightningElement, api, track } from 'lwc';
 
 export default class ProjectPanelLauncher extends LightningElement {
     @api recordId;
+    @track isExpanded = false;
     @track showPanel = false;
     @track activePanelLabel = '';
     @track isTeamPanel = false;
     @track isInteractionsPanel = false;
-    @track isExpanded = false;
 
     get sidebarClass() {
         return this.isExpanded ? 'sidebar expanded' : 'sidebar collapsed';
     }
 
     get toggleIcon() {
-        return this.isExpanded ? 'utility:chevronleft' : 'utility:chevronright';
+        return this.isExpanded ? 'utility:chevrondown' : 'utility:chevronup';
     }
 
     toggleSidebar() {
@@ -21,17 +21,17 @@ export default class ProjectPanelLauncher extends LightningElement {
     }
 
     showTeamPanel() {
-        this.showPanel = true;
         this.activePanelLabel = 'Project Team';
         this.isTeamPanel = true;
         this.isInteractionsPanel = false;
+        this.showPanel = true;
     }
 
     showInteractionsPanel() {
-        this.showPanel = true;
         this.activePanelLabel = 'Project Interactions';
         this.isTeamPanel = false;
         this.isInteractionsPanel = true;
+        this.showPanel = true;
     }
 
     closePanel() {
